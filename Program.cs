9dd8260ed4d24d3afd2ec3909831ee9185a989d9
Erick Cruz <100002211+ErickFPCruz@users.noreply.github.com>
2022-05-26 19:03:17 -0300

    Acabado?

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+int A, B, R;
+
+Console.WriteLine("Algoritmo de Euclides: Método Iterativo MDC\n");
+
+Console.Write("Escreva o dividendo: ");
+A = Convert.ToInt32(Console.ReadLine()!);
+
+Console.Write("Escreva o divisor: ");
+B = Convert.ToInt32(Console.ReadLine()!);
+
+R = A % B;
+Console.Write($"Resto = {R}");
+
+while (B != 0)
+
+{
+A = B;
+B = R;
+}
+return A;
+
+
+Console.WriteLine($"MDC(A,B)= {A}");
